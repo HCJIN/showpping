0b296a6848ee19a96f8e69f16e4f429fa1f6d1c9
HCJIN <ghkdckswls02@naver.com>
2023-04-23 15:00:19 +0900

    second commit

--- src/component/NavBar.js
+++ src/component/NavBar.js
@@ -1,15 +1,34 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faUser } from '@fortawesome/free-solid-svg-icons'
+import { faUser } from '@fortawesome/free-regular-svg-icons'
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
 
 const NavBar = () => {
+  const menuList = ['여성', 'Divided', '남성', '신생아 / 유아', '아동', 'H&M Home', 'Sale', '지속가능성']
   return (
     <div>
         <div>
-            <FontAwesomeIcon icon={faUser} />
+            <div className='login-button'>
+              <FontAwesomeIcon icon={faUser} />
+              <div className='login'>로그인</div>
+            </div>
         </div>
-        <div></div>
-        <div></div>
+        <div className='nav-section'>
+          <img src='https://brandslogo.net/wp-content/uploads/2014/10/h-m-logo.png' alt=''/>
+        </div>
+        <div className='header-area'>
+          <div className='menu-area'>
+            <ul className='menu-List'>
+              {menuList.map((menu) => (
+                <li>{menu}</li>
+              ))}
+            </ul>
+          </div>
+          <div className='search'>
+            <FontAwesomeIcon icon={faMagnifyingGlass} />
+            <input type="text" className="input" value="제품검색"></input>
+          </div>
+        </div>
     </div>
   )
 }
