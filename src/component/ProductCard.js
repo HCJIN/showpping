1ac4901ad51830264cf2067b98461ab59acb2f8f
HCJIN <ghkdckswls02@naver.com>
2023-04-23 16:52:14 +0900

    4 commit

---
+++ src/component/ProductCard.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const ProductCard = () => {
+  return (
+    <div>
+        <img src='https://lp2.hm.com/hmgoepprod?set=source[/4a/05/4a05c82b2b8d1137e46f939477b23e038ea2c2ec.jpg],origin[dam],category[ladies_premium_selection_tops],type[DESCRIPTIVESTILLLIFE],res[w],hmver[2]&call=url[file:/product/main]' alt=''/>
+        <div>Conscious choice</div>
+        <div>벨티드 트윌 코트</div>
+        <div>￦99,900</div>
+        <div>신제품</div>
+    </div>
+  )
+}
+
+export default ProductCard
