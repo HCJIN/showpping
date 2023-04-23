a778dd4a933d9ef4e9cfc9b24b79ac9439e88c83
HCJIN <ghkdckswls02@naver.com>
2023-04-23 14:00:37 +0900

    first commit

---
+++ src/component/NavBar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUser } from '@fortawesome/free-solid-svg-icons'
+
+const NavBar = () => {
+  return (
+    <div>
+        <div>
+            <FontAwesomeIcon icon={faUser} />
+        </div>
+        <div></div>
+        <div></div>
+    </div>
+  )
+}
+
+export default NavBar
