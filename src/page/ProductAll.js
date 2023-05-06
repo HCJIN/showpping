6257b54f69062c4fe045bcea35658878826ffe76
HCJIN <ghkdckswls02@naver.com>
2023-05-06 15:42:27 +0900

    Detail-Page-Change

--- src/page/ProductAll.js
+++ src/page/ProductAll.js
@@ -11,7 +11,7 @@ const ProductAll = () => {
   const getProducts = async () => {
     let searchQuery = query.get('q') || '';
     console.log(searchQuery)
-    let url = `http://localhost:3500/products?q=${searchQuery}`
+    let url = `https://my-json-server.typicode.com/HCJIN/front-hnm/products?q=${searchQuery}`
     let response =  await fetch(url)
     let data = await response.json()
     setProductList(data);
--- src/page/ProductDetail.js
+++ src/page/ProductDetail.js
@@ -2,12 +2,16 @@ import React, { useState } from 'react'
 import { useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import { Col, Container, Row } from 'react-bootstrap'
+import Button from 'react-bootstrap/Button';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import Dropdown from 'react-bootstrap/Dropdown'
+import DropdownButton from 'react-bootstrap/DropdownButton'
 
 const ProductDetail = () => {
   let {id} = useParams();
   const [product, setProduct] = useState(null);
   const getProductDetail = async () => {
-    let url = `http://localhost:3500/products/${id}`;
+    let url = `https://my-json-server.typicode.com/HCJIN/front-hnm/products/${id}`;
     let response = await fetch(url);
     let data = await response.json();
     setProduct(data);
@@ -22,9 +26,18 @@ const ProductDetail = () => {
           <img src={product?.img} />
         </Col>
         <Col>
-          <div>{product?.title}</div>
-          <div>￦{product?.price}</div>
+          <div className='Detail-title'>{product?.title}</div>
+          <div className='Detail-price'>￦{product?.price}</div>
+          <div className='Detail-choice'>{product?.choice == true ? "Conscious choice" : ''}</div>
+          <DropdownButton as={ButtonGroup} title="사이즈 선택" id="bg-vertical-dropdown-3">
+            <Dropdown.Item eventKey="1">S</Dropdown.Item>
+            <Dropdown.Item eventKey="2">M</Dropdown.Item>
+            <Dropdown.Item eventKey="3">L</Dropdown.Item>
+          </DropdownButton>
+          <br/>
+          <Button variant="dark" className='choice-button'>추가</Button>
         </Col>
+
       </Row>
     </Container>
   )
